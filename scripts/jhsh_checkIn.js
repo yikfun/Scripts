0bae9f5acb3a4e48cf14a689865d9743dd6dd0ce
Yikfun <yikfun@163.com>
2023-08-07 10:08:29 +0800

    Update jhsh_checkIn.js

--- scripts/jhsh_checkIn.js
+++ scripts/jhsh_checkIn.js
@@ -10,16 +10,6 @@
 https://raw.githubusercontent.com/FoKit/Scripts/main/boxjs/fokit.boxjs.json
 https://raw.githubusercontent.com/FoKit/Scripts/main/rewrite/get_jhsh_cookie.sgmodule
 
------------------- Surge 配置 -----------------
-
-[MITM]
-hostname = yunbusiness.ccb.com
-
-[Script]
-建行数据 = type=http-request,pattern=^https:\/\/yunbusiness\.ccb\.com\/clp_coupon\/txCtrl\?txcode=A3341A040,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/jhsh_checkIn.js
-
-建行生活 = type=cron,cronexp=17 7 * * *,timeout=60,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/jhsh_checkIn.js,script-update-interval=0
-
 ------------------ Loon 配置 ------------------
 
 [MITM]
